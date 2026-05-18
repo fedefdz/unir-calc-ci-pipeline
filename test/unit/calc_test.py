fa9595590099360a6c51c169fd7cff05f17f7880
Federico Fernandez <fede.fernandez.it@gmail.com>
2026-05-18 15:11:39 +0200

    cover divide by zero branch

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -25,7 +25,10 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
-  
+
+    def test_divide_by_zero_raises_typeerror(self):
+        self.assertRaises(TypeError, self.calc.divide, 10, 0)
+
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
