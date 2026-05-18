d7f644e6a482e6e0ff19c2ccf9f466dea6e2e111
Federico Fernandez <fede.fernandez.it@gmail.com>
2026-05-18 07:25:48 +0200

    api.py: split long lines for flake8

--- app/api.py
+++ app/api.py
@@ -18,8 +18,10 @@ def hello():
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
     try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
+        num_1 = util.convert_to_number(op_1)
+        num_2 = util.convert_to_number(op_2)
+        result = CALCULATOR.add(num_1, num_2)
+        return ("{}".format(result), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
@@ -27,7 +29,9 @@ def add(op_1, op_2):
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
     try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
+        num_1 = util.convert_to_number(op_1)
+        num_2 = util.convert_to_number(op_2)
+        result = CALCULATOR.substract(num_1, num_2)
+        return ("{}".format(result), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
